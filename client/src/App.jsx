d0d787cc93dc6628923947418879aabd33c83756
Louie Alfonso <lfriasalfonso@gmail.com>
2025-01-07 22:26:37 +0000

    Add Layout Components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
 
 function App() {
   
   return (
     <>
-      
+    <main className="w-full min-h-screen bg-[#f3f4f6] ">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+      </Routes>
+    </main> 
     </>
   )
 }
