c8975c830f500c6dc65d47ac5583ffe32715238b
Louie Alfonso <lfriasalfonso@gmail.com>
2025-02-06 22:28:53 +0000

    Update AssetsService

---
+++ client/src/components/layout/MainLayout.tsx
@@ -0,0 +1,13 @@
+
+import { ReactNode } from 'react';
+
+
+const MainLayout = ({ children }: { children: ReactNode }) => {
+  return (
+    <>
+      {children}
+    </>
+  );
+};
+
+export default MainLayout
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,11 +1,14 @@
 import AssetList from "../components/assets/AssetList"
+import MainLayout from "../components/layout/MainLayout"
 
 const Dashboard = () => {
   return (
     <>
-    <AssetList />
+      <MainLayout>
+        <AssetList />
+      </MainLayout>
     </>
-  )
+  );
 }
 
 export default Dashboard
